47536b05dbfd0916808f15a486404cc9e308358c
Martin Cekodhima <mcekodhima@gmail.com>
2018-06-14 09:50:37 +0200

    Altered other pages to be in the same color scheme and theme

---
+++ src/main/webapp/app/welcome/scripts/config.js
@@ -0,0 +1,35 @@
+window.camWelcomeConf = {
+    // change the app name and vendor
+    app: {
+        name: 'Welcome',
+        vendor: 'MPMS'
+    }
+  //
+  // configure the date format
+  // "dateFormat": {
+  //   "normal": "LLL",
+  //   "long":   "LLLL"
+  // },
+  //
+  // "locales": {
+  //    "availableLocales": ["en", "de"],
+  //    "fallbackLocale": "en"
+  //  },
+  // links: [
+  //   {
+  //     label: 'Angular.js Docs',
+  //     href: 'https://code.angularjs.org/1.2.16/docs',
+  //     description: 'Almost interesting'
+  //   },
+  //   {
+  //     label: 'XKCD',
+  //     href: 'http://xkcd.org',
+  //     description: 'Nerdy comic'
+  //   },
+  //   {
+  //     label: 'Slashdot',
+  //     href: 'https://slashdot.org',
+  //     description: 'News for nerds, stuff that matter'
+  //   }
+  // ]
+};
